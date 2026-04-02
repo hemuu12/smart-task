53bfc89b0e1f4f8cef4a5e632e67db2be75f0a44
HariSinghBisht427 <hari@mantechventures.com>
2026-04-02 17:42:05 +0530

    fix url

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 5000;
 const allowedOrigins = [
   'http://localhost:3000',
   'http://localhost:5173',
-  'https://smart-task-3gwn.vercel.app/',
+  'https://smart-task-3gwn.vercel.app',
 ];
 
 const corsOptions: CorsOptions = {
